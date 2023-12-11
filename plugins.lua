426c5acf58e7c1f470a71a443d4d980c7af2afd4
artsR <10280256+artsR@users.noreply.github.com>
2023-12-11 10:03:11 +0000

    Update plugins.lua
    Added placeholders for nvcommunity configurations and for outside plugins

--- plugins.lua
+++ plugins.lua
@@ -60,6 +60,42 @@ local plugins = {
   --   "mg979/vim-visual-multi",
   --   lazy = false,
   -- }
+    --
+  -- Plugins that can be loaded from NvCommunity repo
+  -- {
+  --  "NvChad/nvcommunity",
+  --  { import = "nvcommunity." },
+  -- },
+  --
+  -- Plugins not provided by NvChad repo
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("noice").setup({
+        lsp = {
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true,
+          },
+        },
+        presets = {
+          bottom_search = true,
+          command_palette = true,
+          long_message_to_split = true,
+          inc_rename = false,
+          lsp_doc_border = false,
+        },
+      })
+    end
+  },
+
 }
 
 return plugins
