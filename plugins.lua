8a835a1621e34eac2595aa99ad10c64816580cb2
Artur <pythonarts@op.pl>
2023-12-16 23:42:01 +0100

    Reduce 'noice.nvim' customization to very basic

--- plugins.lua
+++ plugins.lua
@@ -80,17 +80,17 @@ local plugins = {
       require("noice").setup({
         lsp = {
           override = {
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            ["vim.lsp.util.stylize_markdown"] = true,
-            ["cmp.entry.get_documentation"] = true,
+      --       ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      --       ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = false,
           },
         },
         presets = {
-          bottom_search = true,
-          command_palette = true,
+          -- bottom_search = true,
+          -- command_palette = true,
           long_message_to_split = true,
-          inc_rename = false,
-          lsp_doc_border = false,
+          -- inc_rename = false,
+          -- lsp_doc_border = false,
         },
       })
     end
