7c1add2f90dd84c8293085450a1c61344c61a65d
Artur <pythonarts@op.pl>
2023-12-16 23:43:28 +0100

    Add ChatGPT extension

--- plugins.lua
+++ plugins.lua
@@ -96,6 +96,21 @@ local plugins = {
     end
   },
 
+  -- {
+  --   "jackMort/ChatGPT.nvim",
+  --   event = "VeryLazy",
+  --   dependencies = {
+  --     "MunifTanjim/nui.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --     "nvim-telescope/telescope.nvim"
+  --   },
+  --   config = function()
+  --     require("chatgpt").setup({
+  --       api_key_cmd = "echo $OPENAI_API_KEY",
+  --     })
+  --   end
+  -- },
+
 }
 
 return plugins
