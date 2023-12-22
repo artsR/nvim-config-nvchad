af63083101d6b74e39210aa95eb49e3f8ce6ea3e
Warzecha <warzecha@fev.com>
2023-12-22 20:50:23 +0100

    Add camelCase as word feature

--- mappings.lua
+++ mappings.lua
@@ -5,9 +5,15 @@ M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
     ["<leader>sl"] = { "<CMD>SymbolsOutline<CR>", "Toggle list of symbols", opts = { nowait = true } },
+    [",w"] = { "/[A-Z]<CR><CMD>nohlsearch<CR>", "go to the next camelCase", opts = {} },
   },
+
   v = {
     [">"] = { ">gv", "indent"},
+  },
+
+  o = {
+    [",w"] = { "/\\u<CR>", "go to the next camelCase when waiting for movement key"},
   },
 }
 
